071f71006ff474c882e31f1d1cd3783c6993a18b
Serafeim Papastefanos <spapas@gmail.com>
2019-05-17 13:13:11 +0300

    Improve rename proj

--- renameproj.bat
+++ renameproj.bat
@@ -9,3 +9,5 @@ mv lib/%CURRENT_OTP% lib/%NEW_OTP%
 mv lib/%CURRENT_OTP%.ex lib/%NEW_OTP%.ex
 mv lib/%CURRENT_OTP%_web lib/%NEW_OTP%_web
 mv lib/%CURRENT_OTP%_web.ex lib/%NEW_OTP%_web.ex
+mv test/%CURRENT_OTP% test/%NEW_OTP%
+mv test/%CURRENT_OTP%_web test/%NEW_OTP%_web
