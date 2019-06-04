7f8e605dafef3c4f1c88cb3da01de5a983b9b263
Serafeim Papastefanos <spapas@gmail.com>
2019-06-04 09:51:59 +0300

    Fix api for select2

--- lib/phxcrd_web/views/api_view.ex
+++ lib/phxcrd_web/views/api_view.ex
@@ -2,14 +2,13 @@ defmodule PhxcrdWeb.ApiView do
   use PhxcrdWeb, :view
 
   def render("authorities.json", %{authorities: authorities}) do
-    Enum.map(authorities, &authority_json/1)
+    %{results: Enum.map(authorities, &authority_json/1)}
   end
 
   def authority_json(a) do
     %{
       id: a.id,
-      kind: a.authority_kind.name,
-      name: a.name
+      text: "#{a.authority_kind.name} #{a.name}"
     }
   end
 end
