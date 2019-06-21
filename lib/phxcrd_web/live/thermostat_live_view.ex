13c47de04c6684ad23efe4db14429559cc9b425d
Serafeim Papastefanos <spapas@gmail.com>
2019-06-21 12:32:17 +0300

    Finalize the live view

--- lib/phxcrd_web/live/thermostat_live_view.ex
+++ lib/phxcrd_web/live/thermostat_live_view.ex
@@ -6,19 +6,30 @@ defmodule PhxcrdWeb.ThermostatLiveView do
   end
 
   def mount(%{user_id: user_id}, socket) do
+    if connected?(socket), do: :timer.send_interval(500, self(), :update)
+
     case get_user_reading(user_id) do
       {:ok, temperature} ->
-        {:ok, assign(socket, :temperature, temperature)}
+        {:ok, socket |> assign(:temperature, temperature) |> assign(:user_id, temperature)}
 
       {:error, reason} ->
         {:ok, assign(socket, :temperature, reason)}
     end
   end
 
+  def handle_info(:update, socket) do
+    {:ok, temperature} = get_user_reading(socket.assigns.user_id)
+    {:noreply, socket |> assign(:temperature, temperature) |> assign(:user_id, temperature)}
+  end
+
+  def handle_event("reset_temperature", _value, socket) do
+    {:noreply, socket |> assign(:temperature, 0) |> assign(:user_id, 0)}
+  end
+
   defp get_user_reading(user_id) do
     case user_id do
       nil -> {:error, "Please login"}
-      v -> {:ok, v * 100}
+      v -> {:ok, v+1}
     end
   end
 end
