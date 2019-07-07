de77900e29400c8a1db98ae6c28b6dcbdd725e15
Serafeim Papastefanos <spapas@gmail.com>
2019-07-07 19:28:30 +0300

    Upgrade some reqs

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Phxcrd.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.8"},
+      {:phoenix, "~> 1.4.9"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1.2"},
@@ -54,19 +54,18 @@ defmodule Phxcrd.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
+      {:plug_cowboy, "~> 2.1"},
 
       # Extra deps
       {:exldap, "~> 0.6"},
       {:canada, "~> 2.0.0"},
       {:ex_audit, "~> 0.6"},
-
       {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
 
       # {:scrivener, "~> 2.0"},
       {:scrivener_ecto, "~> 2.0"},
       {:scrivener_html, "~> 1.8"},
-      {:sentry, "~> 6.3.0"},
+      {:sentry, "~> 7.1.0"},
       {:bamboo_smtp, "~> 1.6.0"},
       {:pdf_generator, "== 0.5.2"},
       {:elixlsx, "~> 0.4.1"},
