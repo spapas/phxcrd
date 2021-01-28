4365f345fb718c4e8e050e0c2451adc23f98429f
Serafeim Papastefanos <spapas@gmail.com>
2021-01-28 10:59:42 +0200

    Fixes for latest sentry

--- lib/phxcrd_web/endpoint.ex
+++ lib/phxcrd_web/endpoint.ex
@@ -1,4 +1,5 @@
 defmodule PhxcrdWeb.Endpoint do
+  use Sentry.PlugCapture
   use Phoenix.Endpoint, otp_app: :phxcrd
 
   # The session will be stored in the cookie and signed,
@@ -47,6 +48,7 @@ defmodule PhxcrdWeb.Endpoint do
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
 
+  plug Sentry.PlugContext
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
